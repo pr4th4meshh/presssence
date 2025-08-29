85880e723331dd66bdb9f033b21fef956c0942f7
pr4th4meshh <prathameshasolkar17@gmail.com>
2025-08-29 13:44:56 +0530

    fix: invalid dom properties

--- src/app/(routes)/[portfolioUsername]/_components/SharePresssenceButton.tsx
+++ src/app/(routes)/[portfolioUsername]/_components/SharePresssenceButton.tsx
@@ -231,9 +231,9 @@ export default function SharePresssenceButton() {
                             >
                               <path
                                 fill="#212121"
-                                fill-rule="evenodd"
+                                fillRule="evenodd"
                                 d="M38,42H10c-2.209,0-4-1.791-4-4V10c0-2.209,1.791-4,4-4h28	c2.209,0,4,1.791,4,4v28C42,40.209,40.209,42,38,42z"
-                                clip-rule="evenodd"
+                                clipRule="evenodd"
                               ></path>
                               <path
                                 fill="#fff"
@@ -283,9 +283,9 @@ export default function SharePresssenceButton() {
                               ></path>
                               <path
                                 fill="#fff"
-                                fill-rule="evenodd"
+                                fillRule="evenodd"
                                 d="M19.268,16.045c-0.355-0.79-0.729-0.806-1.068-0.82c-0.277-0.012-0.593-0.011-0.909-0.011c-0.316,0-0.83,0.119-1.265,0.594c-0.435,0.475-1.661,1.622-1.661,3.956c0,2.334,1.7,4.59,1.937,4.906c0.237,0.316,3.282,5.259,8.104,7.161c4.007,1.58,4.823,1.266,5.693,1.187c0.87-0.079,2.807-1.147,3.202-2.255c0.395-1.108,0.395-2.057,0.277-2.255c-0.119-0.198-0.435-0.316-0.909-0.554s-2.807-1.385-3.242-1.543c-0.435-0.158-0.751-0.237-1.068,0.238c-0.316,0.474-1.225,1.543-1.502,1.859c-0.277,0.317-0.554,0.357-1.028,0.119c-0.474-0.238-2.002-0.738-3.815-2.354c-1.41-1.257-2.362-2.81-2.639-3.285c-0.277-0.474-0.03-0.731,0.208-0.968c0.213-0.213,0.474-0.554,0.712-0.831c0.237-0.277,0.316-0.475,0.474-0.791c0.158-0.317,0.079-0.594-0.04-0.831C20.612,19.329,19.69,16.983,19.268,16.045z"
-                                clip-rule="evenodd"
+                                clipRule="evenodd"
                               ></path>
                             </svg>
                           </span>
@@ -374,10 +374,10 @@ export default function SharePresssenceButton() {
                                 <path
                                   fill="none"
                                   stroke="#fff"
-                                  stroke-linecap="round"
-                                  stroke-linejoin="round"
-                                  stroke-miterlimit="10"
-                                  stroke-width="2"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  strokeMiterlimit="10"
+                                  strokeWidth="2"
                                   d="M30,11H18c-3.9,0-7,3.1-7,7v12c0,3.9,3.1,7,7,7h12c3.9,0,7-3.1,7-7V18C37,14.1,33.9,11,30,11z"
                                 ></path>
                                 <circle
@@ -394,10 +394,10 @@ export default function SharePresssenceButton() {
                                   r="6"
                                   fill="none"
                                   stroke="#fff"
-                                  stroke-linecap="round"
-                                  stroke-linejoin="round"
-                                  stroke-miterlimit="10"
-                                  stroke-width="2"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  strokeMiterlimit="10"
+                                  strokeWidth="2"
                                 ></circle>
                               </g>
                             </svg>
